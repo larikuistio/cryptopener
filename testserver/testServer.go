65c476296bd99d95eacea8e44786c23b8bc7b507
lari <lari@kuistio.me>
2020-12-02 19:57:04 +0200

    Added test server

---
+++ testserver/testServer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type testServer struct {
+	
+}
+
+var token string
+
+func api(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	tokenJson := "{\"token=" + string(token) + "\"}"
+    switch r.Method {
+    case "GET":
+        w.WriteHeader(http.StatusOK)
+		w.Write([]byte(tokenJson))
+    default:
+        w.WriteHeader(http.StatusNotFound)
+    }
+}
+
+func RandomString(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	token = RandomString(32)
+	fmt.Println("Token is: " + string(token))
+
+    http.HandleFunc("/", api)
+    log.Fatal(http.ListenAndServeTLS("127.0.0.1:8080", "cert.pem", "key.pem", nil))
+}
