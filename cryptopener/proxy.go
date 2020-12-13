b1bf1b8b35af9f440c1913f9a1df9e0364888ac0
lari <lari@kuistio.me>
2020-12-13 18:01:18 +0200

    Rewriting proxy.go

--- cryptopener/proxy.go
+++ cryptopener/proxy.go
@@ -2,61 +2,113 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
-	"io/ioutil"
+	"net"
+	"time"
+	"runtime"
 )
 
 const (
-	LISTEN_HOST = "localhost"
-	LISTEN_PORT = "50000"
-	HTTPSERVER_HOST = "localhost"
+	CLIENT_HOST = "127.0.0.1"
+	CLIENT_PORT = "50000"
+	HTTPSERVER_HOST = "127.0.0.1"
 	HTTPSERVER_PORT = "8080"
 )
 
 func checkError(err error) {
     if err != nil {
-        fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		_, file, no, ok := runtime.Caller(1)
+		if ok {
+			fmt.Printf("\nin: %s#%d\n", file, no)
+		}
+		fmt.Println("\n")
         os.Exit(1)
     }
 }
 
-func main() {
+func clientSocket(ctoschan chan []byte, stocchan chan []byte) {
+	
+	netaddr, err := net.ResolveIPAddr("ip4:ip", CLIENT_HOST)
+	checkError(err)
+
+	conn, err := net.ListenIP("ip4:ipv4", netaddr)
+	checkError(err)
+	
+	go clientSocketWriter(conn, stocchan)
+	go clientSocketReader(conn, ctoschan)
 
 	for {
-		var buf [16384]byte
-
-		tcpAddrc, err := net.ResolveTCPAddr("tcp", LISTEN_HOST + ":" + LISTEN_PORT)
-		checkError(err)
-
-		listener, err := net.ListenTCP("tcp", tcpAddrc)
-		checkError(err)
-		fmt.Println("Listening on " + LISTEN_HOST + ":" + LISTEN_PORT)
-
-		tcpAddrs, err := net.ResolveTCPAddr("tcp", HTTPSERVER_HOST + ":" + HTTPSERVER_PORT)
-		checkError(err)
-
-		conns, err := net.DialTCP("tcp", nil, tcpAddrs)
-		checkError(err)
-		fmt.Println("Connected to http server at " + HTTPSERVER_HOST + ":" + HTTPSERVER_PORT)
-
-		for {
-			connc, err := listener.Accept()
-			checkError(err)
-			
-			_, err = connc.Read(buf[0:])
-			checkError(err)
-
-			_, err = conns.Write([]byte(buf[0:]))
-			checkError(err)
-
-			response, err := ioutil.ReadAll(conns)
-			checkError(err)
-			respLen := len(response)
-			fmt.Printf("Received response of length %d from http server\n", respLen)
-
-			_, err = connc.Write([]byte(response))
-			checkError(err)
-		}
+		time.Sleep(1000 * time.Millisecond)
 	}
+}
+
+func clientSocketWriter(conn *net.IPConn, stocchan chan []byte) {
+	buf := make([]byte, 16384)
+	for {
+		buf = <- stocchan
+		_, err := conn.Write(buf)
+		checkError(err)
+		fmt.Println("clientSocketWriter")
+	}
+}
+
+func clientSocketReader(conn *net.IPConn, ctoschan chan []byte) {
+	buf := make([]byte, 16384)
+	for {
+		_, err := conn.Read(buf)
+		checkError(err)
+		fmt.Println("clientSocketReader")
+		ctoschan <- buf
+	}
+}
+
+func serverSocket(ctoschan chan []byte, stocchan chan []byte) {
+
+	netaddr, err := net.ResolveIPAddr("ip4:ip", HTTPSERVER_HOST)
+	checkError(err)
+
+	conn, err := net.DialIP("ip4:ipv4", nil, netaddr)
+	checkError(err)
+
+	go serverSocketWriter(conn, ctoschan)
+	go serverSocketReader(conn, stocchan)
+
+	for {
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
+
+func serverSocketWriter(conn *net.IPConn, ctoschan chan []byte) {
+	buf := make([]byte, 16384)
+	for {
+		buf = <- ctoschan
+		_, err := conn.Write(buf)
+		checkError(err)
+		fmt.Println("serverSocketWriter")
+	}
+}
+
+func serverSocketReader(conn *net.IPConn, stocchan chan []byte) {
+	buf := make([]byte, 16384)
+	for {
+		_, err := conn.Read(buf)
+		checkError(err)
+		fmt.Println("serverSocketReader")
+		stocchan <- buf
+	}
+}
+
+func main() {
+
+	ctoschan := make(chan []byte)
+	stocchan := make(chan []byte)
+
+	go clientSocket(ctoschan, stocchan)
+	go serverSocket(ctoschan, stocchan)
+
+	for {
+		time.Sleep(1000 * time.Millisecond)
+	}
+
 }
