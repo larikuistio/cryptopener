a7ab44ce9840fa49991c82afee6ec425bd0b6ce4
Juho Sipola <jusipola@student.oulu.fi>
2020-12-20 13:05:36 +0200

    Add url mutator functionality

--- cryptopener/client/client.go
+++ cryptopener/client/client.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"io"
-	"os"
 	"crypto/tls"
+	"fmt"
+	"log"
+	"time"
 )
 
+const ConnectionTimeout = 60
 
 type Client struct {
 	config tls.Config
 	addr string
 	bufferSize int
+	// base attack vector
+	entrypoint string
 }
 
 
 
-func NewClient(addr string) *Client {
+func NewClient(addr string, entrypoint string) *Client {
 	return &Client{
 		addr: addr,
 		bufferSize: 4096,
+		entrypoint: entrypoint,
 		config: tls.Config{
 			// with this we can use our own cert
 			InsecureSkipVerify: true,
@@ -28,16 +31,26 @@ func NewClient(addr string) *Client {
 	}
 }
 
-func (client *Client) SendMessage(message []byte) ([]byte, error) {
+func (client *Client) getRequestBody(message string) []byte {
+	token := fmt.Sprintf("%s%s", client.entrypoint, message)
+	request := fmt.Sprintf("GET %s HTTP/1.1\r\nHost: %s\r\nAccept-Encoding: gzip;deflate\r\n\r\n", token, client.addr)
+	return []byte(request)
+}
+
+func (client *Client) SendMessage(message string) ([]byte, error) {
 	connection, err := tls.Dial("tcp", client.addr, &client.config)
-	log.Printf("Sendig message %b to server", message)
+	// set timeout for connection
+	connection.SetReadDeadline(time.Now().Add(ConnectionTimeout * time.Second))
+
+	log.Printf("Sendig message %s to server", message)
 	if err != nil {
 		log.Fatalf("Failed to create connection, error %e", err)
 		return nil, err
 	}
 	defer connection.Close()
 
-	_, err = connection.Write(message)
+	m := client.getRequestBody(message)
+	_, err = connection.Write(m)
 	if err != nil {
 		log.Fatalln("Failed to write message")
 		return nil, err
@@ -46,24 +59,14 @@ func (client *Client) SendMessage(message []byte) ([]byte, error) {
 	// buffer that contains whole message
 	buffer := make([]byte, 0, client.bufferSize)
 	for {
-		log.Printf("Reading bytes to buffer %b", buffer)
 		b := make([]byte, 1056)
 		size, eof := connection.Read(b)
-		if eof != nil {
-            break
+		if size <= 1056 || eof != nil {
+			buffer = append(buffer, b[:size]...)
+			break
 		}
 		buffer = append(buffer, b[:size]...)
 	}
-	log.Printf("Return buffer %b", buffer)
-	_, e := connection.Read(buffer)
-	if e != nil {
-		log.Fatalf("error %e while reading buffer", e)
-	}
+	log.Printf("Return buffer %s", string(buffer))
 	return buffer, nil
-}
-
-func main() {
-	client := NewClient("127.0.0.1:8080")
-	log.Println(client.SendMessage([]byte("test")))
-	return
 }
