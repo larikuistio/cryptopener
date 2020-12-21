82bffa19ad7f176cb703608405661702da391beb
Juho Sipola <jusipola@student.oulu.fi>
2020-12-21 21:19:39 +0200

    Fix next token position selection

--- cryptopener/mutator.go
+++ cryptopener/mutator.go
@@ -1,54 +1,60 @@
 package cryptopener
 
+import "bytes"
 
-const TOKENS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+// tokens is a array of used tokens
+const tokens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
-type tokenIterator struct {
-	// list of all tokens
-	tokens []byte
-	previous int
-}
-
-func (iterator *tokenIterator) nextToken() byte {
-	next_index := iterator.previous + 1
-	if next_index > len(iterator.tokens) {
-		next_index = 0
-	}
-	next := iterator.tokens[next_index]
-	iterator.previous = next_index
-	return next
-}
-
-// Token mutator creates new token mutations
+// TokenMutator creates new token mutations
 type TokenMutator struct {
-	target string
-	port int
-	previous []byte
-	iterator tokenIterator
-	result []byte
+	// previously used payload
+	previousPayload []byte
+	// correctly quessed tokens
+	result []string
+	// position in the token array
+	index int
+	payloadLength int
 }
 
+// NewMutator creates a new payload mutator
 func NewMutator(target string, port int) *TokenMutator {
 	return &TokenMutator{
-		target: target,
-		port: port,
-		previous: []byte{},
+		index: 0,
+		previousPayload: []byte{},
+		result: []string{}
+		payloadLength: 1
 		iterator: tokenIterator{
-			tokens: []byte(TOKENS),
+			tokens: []byte(tokens),
 			previous: 0,
 		},
 	}
 }
 
-// Create new payload
-func (mutator *TokenMutator) newPayload() ([]byte, error) {
-	// new payload with
-	new_payload := make([]byte, len(mutator.previous) + 1)
+func (mutator TokenMutator) nextToken() byte {
+	// increase index
+	mutator.index += 1
+	remainder := mutator.index % len(tokens)
+	if remainder == 0{
+		mutator.payloadLength += 1
+	}
+	nextToken := mutator.tokens[remainder]
+	return nextToken
+}
 
-	// copy previous payload to new slice
-	copy(new_payload, mutator.previous)
-	new_payload = append(new_payload, mutator.iterator.nextToken())
-	mutator.previous = new_payload
-	return new_payload, nil
+// Creates new payload
+func (mutator *TokenMutator) newPayload(savePrevious bool) ([]byte, error) {
+	nextToken := mutator.nextToken()
+	newPayload := make([]byte, mutator.payloadLength)
+	if mutator.payloadLength > len(mutator.previousPayload) || savePrevious {
+		// copy previous payload to new slice
+		copy(newPayload, mutator.previousPayload)
+		newPayload = append(newPayload, nextToken)
+		mutator.previous = newPayload
+		return newPayload, nil
+	}
+	// FIXME: clean up this mess
+	newPayload = append(newPayload, mutator.previousPayload[:len(mutator.previousPayload - 1)]...)
+	newPayload = append(newPayload, nextToken)
+	return newPayload, nil
 }
