dde0109b254fa802cb43f4f70efaee159dea15bb
Juho Sipola <jusipola@student.oulu.fi>
2020-12-22 22:58:56 +0200

    Fix bugs

--- cryptopener/mutator.go
+++ cryptopener/mutator.go
@@ -1,16 +1,16 @@
 package cryptopener
 
 import (
-	"bytes"
-	"math"
 	"fmt"
+	"log"
+	"math"
 )
 
 // tokens is a array of used tokens
 const tokens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
-func createNewPayload(previuosPayload []byte, length int) []byte {
+func createNewPayload(previousPayload []byte, length int) []byte {
 	newPayload := make([]byte, length)
 	if len(previousPayload) == 0 {
 		return newPayload
@@ -33,40 +33,41 @@ func NewMutator(target string, port int) *TokenMutator {
 	return &TokenMutator{
 		index: 0,
 		previousPayload: []byte{},
-		result: []string{}
-		tokenCount: len(tokens)
-		iterator: tokenIterator{
-			tokens: []byte(tokens),
-			previous: 0,
-		},
+		result: []string{},
+		tokenCount: len(tokens),
 	}
 }
 
 func (mutator TokenMutator) nextToken() byte {
 	// increase index
-	mutator.index += 1
-	nextToken := mutator.tokens[int(mutator.index % mutator.tokenCount)]
+	mutator.index ++
+	nextToken := tokens[int(mutator.index % mutator.tokenCount)]
 	return nextToken
 }
 
 // countPayloadLength counts length of next payload
 func (mutator TokenMutator) countPayloadLength() (int, error) {
 	if mutator.index == 0 {
-		return 0, fmt.Error("Tried to call payload length count with 0")
+		return 0, fmt.Errorf("Tried to call payload length count with 0")
 	}
-	return int(math.Floor(mutator.index % mutator.tokenCount)), nil
+	return int(math.Floor(float64(mutator.index % mutator.tokenCount))), nil
 }
 
 // Creates new payload
 func (mutator *TokenMutator) newPayload(savePrevious bool) ([]byte, error) {
-	nextPayloadLength := mutator.countPayloadLength()
-	newPayload := createNewPayload(mutator.previuosPayload, nextPayloadLength)
+	nextPayloadLength, err := mutator.countPayloadLength()
+	if err != nil {
+		log.Printf("Could not count payload length, errpr %e", err)
+		return nil, err
+	}
+	newPayload := createNewPayload(mutator.previousPayload, nextPayloadLength)
+	nextToken := mutator.nextToken()
 	if nextPayloadLength > len(newPayload) || savePrevious {
 		if savePrevious {
-			mutator.result = append(mutator.result, string(mutator.previuosPayload))
+			mutator.result = append(mutator.result, string(mutator.previousPayload ))
 		}
 		newPayload = append(newPayload, nextToken)
-		mutator.previous = newPayload
+		mutator.previousPayload = newPayload
 		return newPayload, nil
 	}
 	// remove last element from slice
