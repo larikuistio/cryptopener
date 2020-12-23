7945712d013a91ab37500f20c879e2d6810dc220
lari <lari@kuistio.me>
2020-12-23 13:25:47 +0200

    Draft for a way to send request and analyze responses concurrently

--- cryptopener/cryptopener.go
+++ cryptopener/cryptopener.go
@@ -2,6 +2,8 @@ package cryptopener
 
 import (
 	client "github.com/larikuistio/cryptopener/client"
+	"sync"
+	"time"
 )
 
 // Cryptopener a struct that defines cryptopener
@@ -12,6 +14,18 @@ type Cryptopener struct {
 	mutator *TokenMutator
 	ResultToken []string
 }
+
+type ReqRespStruct struct {
+	// request
+	req []byte
+	// response
+	resp []byte
+}
+
+var rmap map[int]ReqRespStruct
+var rctr int
+var actr int
+var mut sync.Mutex
 
 // NewCryptopener creates new instance of Cryptopener
 func NewCryptopener(address string, entry string) *Cryptopener {
@@ -25,24 +39,48 @@ func NewCryptopener(address string, entry string) *Cryptopener {
 	return &cryptopener
 }
 
-func (p *Cryptopener) sendPayload(channel chan []byte, payload []byte) {
-	channel <- []byte{}
+func (p *Cryptopener) sendPayload(payload []byte, client *client.Client) {
+	response := client.SendMessageConcurrent(string(payload))
+	var newstruct ReqRespStruct
+	newstruct.req = payload
+	newstruct.resp = response
+	mut.Lock()
+	rmap[rctr] = newstruct
+	rctr = rctr + 1
+	mut.Unlock()
 	return
 }
 
-func (p *Cryptopener) analyseResponse(response []byte) {
+func (p *Cryptopener) analyseResponse() {
+	for {
+		if actr < rctr {
+			break
+		} else {
+			time.Sleep(1 * time.Millisecond)
+		}
+	}
+	mut.Lock()
+	request := rmap[actr]
+	response := rmap[actr]
+	actr = actr + 1
+	mut.Unlock()
+	// code for analyzing response here
+	_ = response
+	_ = request
 	return
 }
 
 // Run starts BREACH attack
 func (p *Cryptopener) Run() {
-	channel := make(chan []byte, 1)
+	rctr = 0
+	actr = 0
+	client := client.NewClient("127.0.0.1:8080", "asd")
 	for {
 		// create new payload
 		payload, _ := p.mutator.NewPayload(false)
 
 		// send payload into a socket
-		go p.sendPayload(channel, payload)
-		defer p.analyseResponse(<-channel)
+		go p.sendPayload(payload, client)
+		go p.analyseResponse()
 	}
 }
