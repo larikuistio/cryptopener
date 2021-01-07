41cfa6118c4bbe70ada2f765fa1d774a04b00d4a
Juho Sipola <jusipola@student.oulu.fi>
2021-01-07 20:22:51 +0200

    T

--- cryptopener/mutator.go
+++ cryptopener/mutator.go
@@ -1,10 +1,6 @@
 package cryptopener
 
-import (
-	"fmt"
-	"log"
-	"math"
-)
+import "log"
 
 // tokens is a array of used tokens
 const tokens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -19,70 +15,78 @@ func createNewPayload(previousPayload []byte, length int) []byte {
 	return newPayload
 }
 
+type guessToken struct {
+	index int
+}
+
+func (guess *guessToken) nextToken(tokenCount int) byte {
+	nextToken := tokens[int(guess.index % tokenCount)]
+	guess.index++
+	return nextToken
+}
+
+func (guess guessToken) getCurrentToken() byte {
+ 	return tokens[guess.index]
+}
+
+func (guess guessToken) movePosition(tokenCount int) bool {
+	remainder := int(guess.index % tokenCount)
+	if remainder == 0 && guess.index != 0 {
+		return true
+	}
+	return false
+}
+
 // TokenMutator creates new token mutations
 type TokenMutator struct {
 	// previously used payload
 	previousPayload , result[]byte
-	index, tokenCount int
+	previousIndex, tokenCount, position int
+	tokenMap map[int]guessToken
 }
 
 // NewMutator creates a new payload mutator
 func NewMutator() *TokenMutator {
-	return &TokenMutator{
-		index: 0,
+	mutator := TokenMutator{
+		previousIndex: 0,
+		position: 0,
 		previousPayload: []byte{},
 		result: []byte{},
 		tokenCount: len(tokens),
+		tokenMap: make(map[int]guessToken),
 	}
+	// create initial token
+	mutator.tokenMap[0] = guessToken{
+		index: 0,
+	}
+	return &mutator
 }
 
-func (mutator *TokenMutator) NextToken() byte {
-	nextToken := tokens[int(mutator.index % mutator.tokenCount)]
-	mutator.index++
-	return nextToken
-}
-
-// countPayloadLength counts length of next payload
-func (mutator TokenMutator) countPayloadLength() (int, error) {
-	if mutator.index == 0 {
-		return 0, fmt.Errorf("Tried to call payload length count with 0")
+func (mutator *TokenMutator) addToken() {
+	newToken := guessToken{
+		index: 0,
 	}
-	var length int
-	if mutator.index % mutator.tokenCount == 0 {
-		length = int(math.Floor(float64(mutator.index / mutator.tokenCount)))
-	} else {
-		length = int(math.Floor(float64(mutator.index / mutator.tokenCount))) + 1
-	}
-	return length, nil
+	mutator.tokenMap[len(mutator.tokenMap)] = newToken
+	mutator.position = 0
 }
 
 // NewPayload creates new payload
 func (mutator *TokenMutator) NewPayload(savePrevious bool) ([]byte, error) {
-	nextToken := mutator.NextToken()
-	nextPayloadLength, err := mutator.countPayloadLength()
-	if err != nil {
-		log.Printf("Could not count payload length, errpr %e", err)
-		return nil, err
+	if mutator.position >= len(mutator.tokenMap) && mutator.tokenMap[mutator.position].movePosition(mutator.tokenCount) {
+		mutator.addToken()
 	}
 
 	var newPayload []byte
-	if len(mutator.result) != 0 {
-		newPayload = createNewPayload(mutator.result, nextPayloadLength)
-	} else {
-		newPayload = createNewPayload(mutator.previousPayload, nextPayloadLength)
-	}
-
-	if nextPayloadLength > len(mutator.previousPayload) || len(newPayload) == 0 || savePrevious {
-		log.Printf("old new %s %d %d", newPayload, nextPayloadLength, len(mutator.previousPayload))
-		if savePrevious {
-			mutator.result = append(mutator.result, mutator.previousPayload...)
+	for pos, elem := range mutator.tokenMap {
+		if pos == mutator.position {
+			log.Panicf("HERE %d %d %s", pos, mutator.position, string(elem.getCurrentToken()))
+			newPayload = append(newPayload, elem.nextToken(mutator.tokenCount))
+			if len(mutator.tokenMap) > 1 {
+				mutator.position++
+			}
+		} else {
+			newPayload = append(newPayload, elem.getCurrentToken())
 		}
-		newPayload = append(newPayload, nextToken)
-		mutator.previousPayload = newPayload
-		return newPayload, nil
 	}
-	newPayload = newPayload[:len(newPayload) - 1]
-	newPayload = append(newPayload, nextToken)
-	mutator.previousPayload = newPayload
 	return newPayload, nil
 }
